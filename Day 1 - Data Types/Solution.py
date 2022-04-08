9d95db10a1917cc927e43df7234a1baf7f1ed174
Iago Oliveira Dias <iagodois@hotmail.com>
2022-04-08 18:07:23 -0300

    Day 1: Data Types, PY solution.

---
+++ Day 1 - Data Types/Solution.py
@@ -0,0 +1,18 @@
+# Declare second integer, double, and String variables.
+
+i = int(input())
+d = float(input())
+s = input()
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+i += 4
+print(i)
+
+d += 4.0
+print(d)
+
+# Print the sum of the double variables on a new line.
+print("HackerRank {}".format(s))
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
